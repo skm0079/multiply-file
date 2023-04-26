eb8477d60f62d1a5a921a5fabfb119bfb0242b6d
skm0079 <skmukherjee2021@gmail.com>
2023-04-26 21:44:48 +0530

    table aligment issue

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-import pandas as pd
+import uuid
+from faker import Faker
+import random
 from datetime import datetime
 from docxtpl import DocxTemplate
+from docx.shared import Cm
+from docx.enum.table import WD_TABLE_ALIGNMENT
 from docx2pdf import convert
+import pandas as pd
 import yaml
+import pprint
 import os
 
+
+# Generated docx output paths
+generated_docx_paths = []
 
 def convert_docx_to_pdf(input_file_path, output_file_path):
     """
@@ -35,6 +44,42 @@ def convert_docx_to_pdf(input_file_path, output_file_path):
     if not os.path.isfile(output_file_path):
         raise RuntimeError("Failed to create pdf output file.")
 
+def add_table_to_doc(doc, rows, cols):
+    # Initialize faker object
+    fake = Faker()
+
+    # Create table with given rows and columns
+    table = doc.add_table(rows=rows, cols=cols)
+
+    # Set table alignment to center
+    table.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+    # Set column widths
+    col_widths = [Cm(2), Cm(3), Cm(4), Cm(2), Cm(2), Cm(3)]
+    for i, width in enumerate(col_widths):
+        table.columns[i].width = width
+
+    # Set table header row
+    hdr_cells = table.rows[0].cells
+    hdr_cells[0].text = 'Date'
+    hdr_cells[1].text = 'Referal code'
+    hdr_cells[2].text = 'Mode of trasaction'
+    hdr_cells[3].text = 'Debit'
+    hdr_cells[4].text = 'Credit'
+    hdr_cells[5].text = 'Total balance'
+
+    # Populate table with random data
+    for i in range(1, rows):
+        row_cells = table.rows[i].cells
+        row_cells[0].text = fake.date_this_month().strftime('%d-%m-%Y')
+        row_cells[1].text = str(uuid.UUID(fake.uuid4()).hex)[:8]
+        row_cells[2].text = random.choice(['Online', 'Offline'])
+        row_cells[3].text = str(random.randint(1, 10000))
+        row_cells[4].text = str(random.randint(1, 10000))
+        row_cells[5].text = str(random.randint(1, 100000))
+
+    # Add spacing after the table
+    doc.add_paragraph(' ')
 
 def generate_document_and_pdf(template_path, data, output_dir):
     """
@@ -71,12 +116,15 @@ def generate_document_and_pdf(template_path, data, output_dir):
             'debit': row['debit'],
             'credit': row['credit'],
             'total_balance': row['total_balance'],
-            'today_date': datetime.today().strftime("%d %b, %Y")
+            'today_date': datetime.today().strftime("%d %b, %Y"),
         }
 
         # Load the document template and render it with the context
         doc = DocxTemplate(template_path)
         doc.render(context)
+
+        # Add table to the document
+        add_table_to_doc(doc, 10, 6) # Example values for 10 rows and 6 columns
 
         # Define output file paths
         docx_output_path = os.path.join(output_dir, f"generated_doc_{input_yaml_data['file_name']}_{index}.docx")
@@ -85,6 +133,9 @@ def generate_document_and_pdf(template_path, data, output_dir):
         try:
             # Save the document as a docx file
             doc.save(docx_output_path)
+
+            # Append to List of Output paths 
+            generated_docx_paths.append(docx_output_path)
 
             # Convert the docx file to pdf
             convert_docx_to_pdf(docx_output_path, pdf_output_path)
@@ -111,10 +162,5 @@ if __name__ == '__main__':
 
     # Generate documents and pdfs
     generate_document_and_pdf(template_path, data, output_doc_dir)
-
-    # Convert all generated docx files to pdf
-    for file_name in os.listdir(output_doc_dir):
-        if file_name.endswith('.docx'):
-            file_path = os.path.join(output_doc_dir, file_name)
-            convert_docx_to_pdf(file_path, os.path.join(output_pdf_dir, file_name.replace('.docx', '.pdf')))
+    
 
