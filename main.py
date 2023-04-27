b7d3cde1c9dbf191bde661c00ed23fcb6cdffb4c
skm0079 <skmukherjee2021@gmail.com>
2023-04-27 10:58:16 +0530

    table at the end of the document

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uuid
 from faker import Faker
 import random
+import docx
 from datetime import datetime
 from docxtpl import DocxTemplate
 from docx.shared import Cm
@@ -11,9 +12,6 @@ import yaml
 import pprint
 import os
 
-
-# Generated docx output paths
-generated_docx_paths = []
 
 def convert_docx_to_pdf(input_file_path, output_file_path):
     """
@@ -44,42 +42,77 @@ def convert_docx_to_pdf(input_file_path, output_file_path):
     if not os.path.isfile(output_file_path):
         raise RuntimeError("Failed to create pdf output file.")
 
-def add_table_to_doc(doc, rows, cols):
-    # Initialize faker object
-    fake = Faker()
 
-    # Create table with given rows and columns
-    table = doc.add_table(rows=rows, cols=cols)
+def add_table_to_doc(doc_path, placeholder_text, rows, cols):
+    # Open the docx file
+    doc = docx.Document(doc_path)
 
-    # Set table alignment to center
-    table.alignment = WD_TABLE_ALIGNMENT.CENTER
+    # Before creating the table
+    print(f"Number of tables before: {len(doc.tables)}")
 
-    # Set column widths
-    col_widths = [Cm(2), Cm(3), Cm(4), Cm(2), Cm(2), Cm(3)]
-    for i, width in enumerate(col_widths):
-        table.columns[i].width = width
+    # Find the placeholder
+    placeholder = None
+    for paragraph in doc.paragraphs:
+        if placeholder_text in paragraph.text:
+            placeholder = paragraph
+            break
 
-    # Set table header row
-    hdr_cells = table.rows[0].cells
-    hdr_cells[0].text = 'Date'
-    hdr_cells[1].text = 'Referal code'
-    hdr_cells[2].text = 'Mode of trasaction'
-    hdr_cells[3].text = 'Debit'
-    hdr_cells[4].text = 'Credit'
-    hdr_cells[5].text = 'Total balance'
+    # If placeholder is found, replace it with the table
+    if placeholder:
+        # Remove the placeholder paragraph
+        p = placeholder._element
+        p.getparent().remove(p)
+        p._p = p._element = None
 
-    # Populate table with random data
-    for i in range(1, rows):
-        row_cells = table.rows[i].cells
-        row_cells[0].text = fake.date_this_month().strftime('%d-%m-%Y')
-        row_cells[1].text = str(uuid.UUID(fake.uuid4()).hex)[:8]
-        row_cells[2].text = random.choice(['Online', 'Offline'])
-        row_cells[3].text = str(random.randint(1, 10000))
-        row_cells[4].text = str(random.randint(1, 10000))
-        row_cells[5].text = str(random.randint(1, 100000))
+        # Insert an empty paragraph before the table
+        placeholder.insert_paragraph_before('Tomcat')
 
-    # Add spacing after the table
-    doc.add_paragraph(' ')
+        # Create the table
+        table = doc.add_table(rows=rows, cols=cols)
+
+        # Print the text of the paragraph before the table
+        print(table._element.getprevious().text)
+
+        # After creating the table
+        print(f"Number of tables after: {len(doc.tables)}")
+
+        # Set table alignment to center
+        table.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+        # Set column widths
+        col_widths = [Cm(2), Cm(3), Cm(4), Cm(2), Cm(2), Cm(3)]
+        for i, width in enumerate(col_widths):
+            table.columns[i].width = width
+
+        # Set table header row
+        hdr_cells = table.rows[0].cells
+        hdr_cells[0].text = 'Date'
+        hdr_cells[1].text = 'Referal code'
+        hdr_cells[2].text = 'Mode of transaction'
+        hdr_cells[3].text = 'Debit'
+        hdr_cells[4].text = 'Credit'
+        hdr_cells[5].text = 'Total balance'
+
+        # Populate table with random data
+        fake = Faker()
+        for i in range(1, rows):
+            row_cells = table.rows[i].cells
+            row_cells[0].text = fake.date_this_month().strftime('%d-%m-%Y')
+            row_cells[1].text = str(uuid.UUID(fake.uuid4()).hex)[:8]
+            row_cells[2].text = random.choice(['Online', 'Offline'])
+            row_cells[3].text = str(random.randint(1, 10000))
+            row_cells[4].text = str(random.randint(1, 10000))
+            row_cells[5].text = str(random.randint(1, 100000))
+
+        # Add spacing after the table
+        doc.add_paragraph(' ')
+
+        # Save the modified docx file
+        doc.save(doc_path)
+
+        print(f"Table successfully added to {doc_path}!")
+    else:
+        print(f"Placeholder '{placeholder_text}' not found in {doc_path}!")
 
 def generate_document_and_pdf(template_path, data, output_dir):
     """
@@ -123,9 +156,6 @@ def generate_document_and_pdf(template_path, data, output_dir):
         doc = DocxTemplate(template_path)
         doc.render(context)
 
-        # Add table to the document
-        add_table_to_doc(doc, 10, 6) # Example values for 10 rows and 6 columns
-
         # Define output file paths
         docx_output_path = os.path.join(output_dir, f"generated_doc_{input_yaml_data['file_name']}_{index}.docx")
         pdf_output_path = os.path.join(output_dir, f"generated_doc_{input_yaml_data['file_name']}_{index}.pdf")
@@ -133,9 +163,8 @@ def generate_document_and_pdf(template_path, data, output_dir):
         try:
             # Save the document as a docx file
             doc.save(docx_output_path)
-
-            # Append to List of Output paths 
-            generated_docx_paths.append(docx_output_path)
+            # Add table to the document
+            add_table_to_doc(docx_output_path,'abcxyz', 10, 6) # Example values for 10 rows and 6 columns
 
             # Convert the docx file to pdf
             convert_docx_to_pdf(docx_output_path, pdf_output_path)
