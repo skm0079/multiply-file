d9cafb791105a0b7ff5ba31adcf908cd904f3871
skm0079 <skmukherjee2021@gmail.com>
2023-04-28 16:08:09 +0530

    Main updated with raw borderless table code

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import pandas as pd
+import uuid
+from faker import Faker
+import random
+import docx
 from datetime import datetime
 from docxtpl import DocxTemplate
+from docx.shared import Cm
+from docx.enum.table import WD_TABLE_ALIGNMENT
 from docx2pdf import convert
+import pandas as pd
 import yaml
+import pprint
 import os
 
 
@@ -35,6 +42,77 @@ def convert_docx_to_pdf(input_file_path, output_file_path):
     if not os.path.isfile(output_file_path):
         raise RuntimeError("Failed to create pdf output file.")
 
+
+def add_table_to_doc(doc_path, placeholder_text, rows, cols):
+    # Open the docx file
+    doc = docx.Document(doc_path)
+
+    # Before creating the table
+    print(f"Number of tables before: {len(doc.tables)}")
+
+    # Find the placeholder
+    placeholder = None
+    for paragraph in doc.paragraphs:
+        if placeholder_text in paragraph.text:
+            placeholder = paragraph
+            break
+
+    # If placeholder is found, replace it with the table
+    if placeholder:
+        # Remove the placeholder paragraph
+        p = placeholder._element
+        p.getparent().remove(p)
+        p._p = p._element = None
+
+        # Insert an empty paragraph before the table
+        placeholder.insert_paragraph_before('Tomcat')
+
+        # Create the table
+        table = doc.add_table(rows=rows, cols=cols)
+
+        # Print the text of the paragraph before the table
+        print(table._element.getprevious().text)
+
+        # After creating the table
+        print(f"Number of tables after: {len(doc.tables)}")
+
+        # Set table alignment to center
+        table.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+        # Set column widths
+        col_widths = [Cm(2), Cm(3), Cm(4), Cm(2), Cm(2), Cm(3)]
+        for i, width in enumerate(col_widths):
+            table.columns[i].width = width
+
+        # Set table header row
+        hdr_cells = table.rows[0].cells
+        hdr_cells[0].text = 'Date'
+        hdr_cells[1].text = 'Referal code'
+        hdr_cells[2].text = 'Mode of transaction'
+        hdr_cells[3].text = 'Debit'
+        hdr_cells[4].text = 'Credit'
+        hdr_cells[5].text = 'Total balance'
+
+        # Populate table with random data
+        fake = Faker()
+        for i in range(1, rows):
+            row_cells = table.rows[i].cells
+            row_cells[0].text = fake.date_this_month().strftime('%d-%m-%Y')
+            row_cells[1].text = str(uuid.UUID(fake.uuid4()).hex)[:8]
+            row_cells[2].text = random.choice(['Online', 'Offline'])
+            row_cells[3].text = str(random.randint(1, 10000))
+            row_cells[4].text = str(random.randint(1, 10000))
+            row_cells[5].text = str(random.randint(1, 100000))
+
+        # Add spacing after the table
+        doc.add_paragraph(' ')
+
+        # Save the modified docx file
+        doc.save(doc_path)
+
+        print(f"Table successfully added to {doc_path}!")
+    else:
+        print(f"Placeholder '{placeholder_text}' not found in {doc_path}!")
 
 def generate_document_and_pdf(template_path, data, output_dir):
     """
@@ -71,7 +149,7 @@ def generate_document_and_pdf(template_path, data, output_dir):
             'debit': row['debit'],
             'credit': row['credit'],
             'total_balance': row['total_balance'],
-            'today_date': datetime.today().strftime("%d %b, %Y")
+            'today_date': datetime.today().strftime("%d %b, %Y"),
         }
 
         # Load the document template and render it with the context
@@ -85,6 +163,8 @@ def generate_document_and_pdf(template_path, data, output_dir):
         try:
             # Save the document as a docx file
             doc.save(docx_output_path)
+            # Add table to the document
+            add_table_to_doc(docx_output_path,'Transaction Table', 10, 6) # Example values for 10 rows and 6 columns
 
             # Convert the docx file to pdf
             convert_docx_to_pdf(docx_output_path, pdf_output_path)
@@ -111,5 +191,5 @@ if __name__ == '__main__':
 
     # Generate documents and pdfs
     generate_document_and_pdf(template_path, data, output_doc_dir)
-
+    
 
